74a06fa31ea2fd79750e2f9cf7c4cfd91e05a737
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-03-15 11:17:46 +0300

    Starting with tkinter

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Text Editor")  # title of window
+root.geometry("1200x620+10+10")  # window size
+root.resizable(False, False)    # don't make it  resizable
+
+root.mainloop()
