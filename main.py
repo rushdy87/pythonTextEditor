2e163d06728015859d25733d415b79be9304ef1a
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-03-22 04:15:56 +0300

    cut, copy, and past

--- main.py
+++ main.py
@@ -164,35 +164,6 @@ filemenu.add_separator()
 filemenu.add_command(label='Exit', accelerator='Ctrl+Q', command=exit_app)
 menubar.add_cascade(label='File', menu=filemenu)
 
-# Edit menu
-editmenu = Menu(menubar, tearoff=False)
-editmenu.add_command(label='Cut', accelerator='Ctrl+X')
-editmenu.add_command(label='Copy', accelerator='Ctrl+C')
-editmenu.add_command(label='Paste', accelerator='Ctrl+V')
-editmenu.add_command(label='Clear', accelerator='Ctrl+Alt+X')
-editmenu.add_command(label='Find', accelerator='Ctrl+F')
-menubar.add_cascade(label='Edit', menu=editmenu)
-
-# View menu
-show_toolbar = BooleanVar()
-show_statusbar = BooleanVar()
-viewmenu = Menu(menubar, tearoff=False)
-viewmenu.add_checkbutton(
-    label='Tool Bar', variable=show_toolbar, onvalue=True, offvalue=False)
-viewmenu.add_checkbutton(
-    label='Status Bar', variable=show_statusbar, onvalue=True, offvalue=False)
-menubar.add_cascade(label='View', menu=viewmenu)
-
-# Themes menu
-thememenu = Menu(menubar, tearoff=False)
-theme_choice = StringVar()
-light_image = PhotoImage(file='images/light.gif')
-thememenu.add_radiobutton(label='Light', image=light_image,
-                          variable=theme_choice, compound=LEFT)
-dark_image = PhotoImage(file='images/dark.gif')
-thememenu.add_radiobutton(
-    label='Dark Default', image=dark_image, variable=theme_choice, compound=LEFT)
-menubar.add_cascade(label='Theme', menu=thememenu)
 
 # Toolbar section
 tool_bar = Label(root)
@@ -252,5 +223,35 @@ status_bar = Label(root, text='Status Bar')
 status_bar.pack(side=BOTTOM)
 
 textArea.bind('<<Modified>>', status_bar_function)
+
+# Edit menu
+editmenu = Menu(menubar, tearoff=False)
+editmenu.add_command(label='Cut', accelerator='Ctrl+X', command=lambda: textArea.event_generate('<Control x>'))
+editmenu.add_command(label='Copy', accelerator='Ctrl+C', command=lambda: textArea.event_generate('<Control c>'))
+editmenu.add_command(label='Paste', accelerator='Ctrl+V', command=lambda: textArea.event_generate('<Control v>'))
+editmenu.add_command(label='Clear', accelerator='Ctrl+Alt+X')
+editmenu.add_command(label='Find', accelerator='Ctrl+F')
+menubar.add_cascade(label='Edit', menu=editmenu)
+
+# View menu
+show_toolbar = BooleanVar()
+show_statusbar = BooleanVar()
+viewmenu = Menu(menubar, tearoff=False)
+viewmenu.add_checkbutton(
+    label='Tool Bar', variable=show_toolbar, onvalue=True, offvalue=False)
+viewmenu.add_checkbutton(
+    label='Status Bar', variable=show_statusbar, onvalue=True, offvalue=False)
+menubar.add_cascade(label='View', menu=viewmenu)
+
+# Themes menu
+thememenu = Menu(menubar, tearoff=False)
+theme_choice = StringVar()
+light_image = PhotoImage(file='images/light.gif')
+thememenu.add_radiobutton(label='Light', image=light_image,
+                          variable=theme_choice, compound=LEFT)
+dark_image = PhotoImage(file='images/dark.gif')
+thememenu.add_radiobutton(
+    label='Dark Default', image=dark_image, variable=theme_choice, compound=LEFT)
+menubar.add_cascade(label='Theme', menu=thememenu)
 
 root.mainloop()
